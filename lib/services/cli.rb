731b1394ce80083b71665c57d29310ce029eac1f
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-07-09 22:21:45 -0700

    cli main menu set up

--- lib/services/cli.rb
+++ lib/services/cli.rb
@@ -2,6 +2,12 @@
 
 class CLI 
 
+  attr_accessor :input 
+
+  def initialize 
+    @input = ""
+  end 
+
   def run 
     welcome
     #sleep (1)
@@ -9,7 +15,25 @@ class CLI
   end 
 
   def welcome
-    puts "Welcome to Brewdogs' Assistant!"
-    puts "Let's Party!"
+    puts "Welcome to Brewdogs' Assistant!".black.on_green
+    puts "Let's Party!".black.on_green
+  end 
+
+  def main_menu
+    prompts
+    while input.downcase != "q" do 
+      case input.downcase 
+      when "y"
+        Beer.random_beer
+      else 
+        puts "Invalid entry, try again"
+      end 
+    end 
+  end 
+
+  def prompts 
+    puts ""
+    puts "Hit 'Y' for Brewer's Pick"
+    puts ""
   end 
 end 

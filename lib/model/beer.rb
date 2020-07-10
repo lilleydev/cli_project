0b63b5f8dbe386c4e46b51e1f3163357181ff2b2
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-07-09 20:06:07 -0700

    beer class initialization

--- lib/model/beer.rb
+++ lib/model/beer.rb
@@ -1,3 +1,17 @@
 class Beer
+  attr_accessor :name, :city, :state, :food_pairing, :description, :brewers_tips, :hops, :input
 
+  def initialize(name, food_pairing, description)
+    @name = name
+    #@brewers_tips = brewers_tips
+    @food_pairing = food_pairing
+    @description = description
+    #@ingredients = ingredients
+    @@all << self
+  end 
+
+  def self.all
+    @@all 
+  end
+   
 end
